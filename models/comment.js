92a6aabeb8be065d771b6a11000a5fe0f1f5500e
Gourav <gauravdeswal06@gmail.com>
2020-04-21 16:33:40 +0530

    create mongoose schema for post, comment and user

---
+++ models/comment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const commmentSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    Post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Post'
+    }
+},{timestamps:true});
+
+const Comment = mongoose.model('Comment',commmentSchema);
+module.exports = Comment;
