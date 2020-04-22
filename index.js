8104fbbe833b414e73f5bb6545e3566fa64cd0ee
Gourav <gauravdeswal06@gmail.com>
2020-04-22 11:54:29 +0530

    Set up partials

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const Port = 8000;
 const db = require('./config/mongoose');
 
 
+
 app.use('/',require('./routes/index'));
+app.use(express.static('./assets'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(Port, function(err){
     if(err){
