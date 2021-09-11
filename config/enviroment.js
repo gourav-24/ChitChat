58d818899f28f88c6f6f178b9c5547d38775ff1a
Gourav <gauravdeswal06@gmail.com>
2021-09-12 01:02:54 +0530

    changed google auth 2 call back url

--- config/enviroment.js
+++ config/enviroment.js
@@ -5,7 +5,7 @@ const development ={
     db:'Chit-Chat',
     client_ID :"465922811237-fpsjo12qk2oi3p925eelbd405m35pksf.apps.googleusercontent.com",
     client_Secret:"u4kQxDr69hkwxWO6BGw4_49Y",
-    callback_URL:"http://localhost:8000/users/auth/google/callback"
+    callback_URL:"https://chit-chat-v1.herokuapp.com/users/auth/google/callback"
 
 
 }
